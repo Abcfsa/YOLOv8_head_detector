66f9b99de27851fa759a4a2c6e5b27b97abd318b
yujie <zhaoyujie20a@mails.ucas.ac.cn>
2023-11-13 06:10:36 -0500

    new mode

--- example.py
+++ example.py
@@ -11,6 +11,8 @@ def get_parser():
                       help='path to the set of pics to detect heads from,must be a folder for now')
   parser.add_argument('--output',type=str,default="./output_%d.csv"%num,
                       help='path to the output_file.')
+  parser.add_argument('--mode',type=str,default="detect",
+                      help='mode of the model:detect or track.')
   return parser
   
 if __name__ == "__main__":
@@ -18,11 +20,25 @@ if __name__ == "__main__":
   args = parser.parse_args()
   model=YOLO(args.model)
   #pics=os.listdir(args.source)
-  df=pd.DataFrame(data=None,columns=['name','xmin','ymin','xmax','ymax'])
-  results=model(args.source,stream=True)
-  for result in results:
-    box=result.boxes.cpu().numpy()
-    for i in range(box.shape[0]):
-      data=[result.path.split("/")[-1]]+list(box.xyxyn[i])
-      df.loc[df.shape[0]]=data
-  df.to_csv(args.output,index=False)
+  if args.mode=='detect':
+    df=pd.DataFrame(data=None,columns=['name','xmin','ymin','xmax','ymax'])
+    results=model(args.source,stream=True)
+    for result in results:
+      box=result.boxes.cpu().numpy()
+      for i in range(box.shape[0]):
+        data=[result.path.split("/")[-1]]+list(box.xyxyn[i])
+        df.loc[df.shape[0]]=data
+    df.to_csv(args.output,index=False)
+  elif args.mode=='track':
+    df=pd.DataFrame(data=None,columns=['name','id','xmin','ymin','xmax','ymax'])
+    results=model.track(args.source,stream=True,persist=True)
+    for result in results:
+      box=result.boxes.cpu().numpy()
+      for i in range(box.shape[0]):
+        if box.is_track is True:
+          data=[result.path.split("/")[-1]]+[int(box.id[i])]+list(box.xyxyn[i])
+          df.loc[df.shape[0]]=data
+    df.to_csv(args.output,index=False)
+  else:
+    print("Please enter a proper mode.")
+  
